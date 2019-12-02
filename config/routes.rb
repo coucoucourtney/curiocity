3401acceb43a1a580bdf7469e67ff7b0121b0e06
mescal <achung020@gmail.com>
2019-12-02 18:46:27 +0800

    final push for the day

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update, :destroy]
-      #     resources :buildings, only: [:index, :show, :create, :update, :destroy]
-      #     resources :checkpoints, only: [:index, :show, :create, :update, :destroy]
-      #     resources :routes, only: [:index, :show, :create, :update, :destroy]
+      resources :buildings, only: [:index, :show, :create, :update, :destroy]
+      resources :routes, only: [:index, :show, :create, :update, :destroy]
     end
   end
   post '/login', to: 'login#login'
