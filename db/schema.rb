81bab998919e2966e42afe6bbcd4d17c757cde32
Maxime Bruguier <maxime@azel.co>
2019-12-05 18:32:44 +0800

    favorite method added to buildings controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_034540) do
+ActiveRecord::Schema.define(version: 2019_12_05_101929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2019_12_04_034540) do
     t.string "city"
     t.boolean "fact_checked"
     t.string "district"
-    t.string "favoritable_total"
-    t.string "favoritable_score"
+    t.text "favoritable_total"
+    t.text "favoritable_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_buildings_on_user_id"
@@ -101,10 +101,10 @@ ActiveRecord::Schema.define(version: 2019_12_04_034540) do
     t.string "email"
     t.string "language"
     t.integer "gender"
-    t.string "favoritor_score"
-    t.string "favoritor_total"
-    t.string "favoritable_score"
-    t.string "favoritable_total"
+    t.text "favoritor_score"
+    t.text "favoritor_total"
+    t.text "favoritable_score"
+    t.text "favoritable_total"
   end
 
   add_foreign_key "buildings", "users"
