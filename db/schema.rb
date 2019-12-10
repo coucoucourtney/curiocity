465b5958c98bf4d0d146f3a477f889f2135d33f0
Courtney Wagner <cwagn@me.com>
2019-12-10 16:40:57 +0800

    added user type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_042004) do
+ActiveRecord::Schema.define(version: 2019_12_10_081446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_042004) do
     t.text "favoritable_score"
     t.text "favoritable_total"
     t.boolean "admin", default: false, null: false
+    t.string "user_type", default: "user"
   end
 
   add_foreign_key "buildings", "users"
